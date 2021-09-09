b56745aeee7fd4705960ab300de4c01c7a188348
joao <joaopedro37@gmail.com>
2021-09-09 16:53:46 -0300

    BD feito!

---
+++ BD/Inlock_03_DQL.sql
@@ -0,0 +1,35 @@
+USE inlock_games_tarde;
+GO
+
+--Listar todos os usuários
+SELECT * FROM USUARIO
+--Listar todos os estúdios
+SELECT * FROM ESTUDIO
+--Listar todos os jogos
+SELECT * FROM JOGO
+--TipoUsuario
+SELECT * FROM TIPOUSUARIO
+
+--Listar todos os jogos e seus respectivos estúdios
+SELECT idJogo,nomeJogo [Nome Jogo],descrição,dataLancamento [Data de Lançamento],valor,nomeEstudio[Nome Estudio] FROM JOGO J
+INNER JOIN ESTUDIO E ON J.idEstudio = E.idEstudio
+
+--Buscar e trazer na lista todos os estúdios com os respectivos jogos. Obs.: Listar
+--todos os estúdios mesmo que eles não contenham nenhum jogo de referência
+SELECT E.idEstudio,nomeEstudio[Nome Estudio],nomeJogo [Nome Jogo],descrição,dataLancamento [Data de Lançamento],valor FROM ESTUDIO E
+LEFT JOIN JOGO J ON E.idEstudio = J.idEstudio
+
+
+--Buscar um usuário por e-mail e senha (login)
+SELECT * FROM USUARIO
+WHERE email = 'cliente@cliente.com'
+AND SENHA = 'cliente'
+
+--Buscar um jogo por idJogo
+SELECT * FROM JOGO
+WHERE idJogo = 2
+
+--Buscar um estúdio por idEstudio
+SELECT * FROM ESTUDIO
+WHERE idEstudio = 3
+
